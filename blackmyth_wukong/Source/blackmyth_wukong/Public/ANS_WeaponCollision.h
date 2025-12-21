016c9f75df3e1de8046b32f1262724cd0a657669
epiphany <jasonyan0019@gmail.com>
2025-12-21 21:38:20 +0800

    添加Grux武器碰撞体、伤害判定及动画、受击判定及动画、死亡动画

---
+++ blackmyth_wukong/Source/blackmyth_wukong/Public/ANS_WeaponCollision.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ANS_WeaponCollision.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKMYTH_WUKONG_API UANS_WeaponCollision : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
