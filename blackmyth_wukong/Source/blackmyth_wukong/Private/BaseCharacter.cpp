d1cf92f7630baca84f23a19a6897648264c53846
epiphany <jasonyan0019@gmail.com>
2025-12-27 23:01:17 +0800

    修复"技能4-变身"UI显示异常问题

--- blackmyth_wukong/Source/blackmyth_wukong/Private/BaseCharacter.cpp
+++ blackmyth_wukong/Source/blackmyth_wukong/Private/BaseCharacter.cpp
@@ -104,8 +104,7 @@ float ABaseCharacter::GetCooldownPercent() const
 {
 	if (bIsCooldown && GetWorldTimerManager().IsTimerActive(CooldownTimerHandle))
 	{
-		float Remaining = GetWorldTimerManager().GetTimerRemaining(CooldownTimerHandle);
-		return FMath::Clamp(Remaining / TransformCooldownDuration, 0.0f, 1.0f);
+		return 1.0f - FMath::Clamp(GetWorldTimerManager().GetTimerRemaining(CooldownTimerHandle) / TransformCooldownDuration, 0.0f, 1.0f);
 	}
-	return 0.0f;
+	return 1.0f;
 }
